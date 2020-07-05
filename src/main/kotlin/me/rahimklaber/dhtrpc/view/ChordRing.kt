b02b7626f9bc027f6e5656da6be2acffbafc66b1
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-07-05 16:45:28 +0200

    Added ability to view all table data of a node

--- src/main/kotlin/me/rahimklaber/dhtrpc/view/ChordRing.kt
+++ src/main/kotlin/me/rahimklaber/dhtrpc/view/ChordRing.kt
@@ -7,19 +7,20 @@ import javafx.collections.FXCollections
 import javafx.collections.MapChangeListener
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
+import javafx.scene.control.TableView
 import javafx.scene.control.TextArea
-import javafx.scene.input.KeyCode
 import javafx.util.Duration
 import me.rahimklaber.dhtrpc.ChordNode
 import me.rahimklaber.dhtrpc.Services.tableEntry
 import tornadofx.*
 
+data class MyPair<F, S>(var first: F, var second: S)
 data class UiTableEntry(val tableEntry: tableEntry, val fingerPos: Int)
 data class DataEntry(val name: String, val data: String)
 class ChordRing : View("ChordRing") {
-//    val args = arrayOf("192.168.0.175", "222")
+    //    val args = arrayOf("192.168.0.175", "222")
 //
-    val args = arrayOf("192.168.0.175", "222", "192.168.0.187", "545")
+    val args = arrayOf("192.168.0.175", "222", "192.168.0.187", "554")
     var node: ChordNode = ChordNode.create(args)
     val map = node.fingerTable
     val datamap = node.dataTable
@@ -46,25 +47,51 @@ class ChordRing : View("ChordRing") {
             timeline.play()
 
         }
-        left = tableview<UiTableEntry> {
-            items = FXCollections.observableArrayList(map.map { e -> UiTableEntry(e.value, e.key) })
-            map.addListener(MapChangeListener {
-                runLater { items = FXCollections.observableArrayList(map.map { e -> UiTableEntry(e.value, e.key) }) }
-            })
-            val poscol = readonlyColumn("fingerpos", UiTableEntry::fingerPos)
-            readonlyColumn("entry", UiTableEntry::tableEntry)
+        right = tableview<MyPair<String, String?>> {
+            readonlyColumn("key", MyPair<String, String?>::first)
+            readonlyColumn("value", MyPair<String, String?>::second)
+            setOnMouseClicked {
+                runAsyncWithOverlay {
+                    val data = node.getRequest(this.selectedItem!!.first)
+                    this.selectedItem!!.second = data.data
+                    this.refresh()
+                }
+            }
+        }
+        left = hbox {
+            tableview<UiTableEntry> {
+                items = FXCollections.observableArrayList(map.map { e -> UiTableEntry(e.value, e.key) })
+                map.addListener(MapChangeListener {
+                    runLater {
+                        items = FXCollections.observableArrayList(map.map { e -> UiTableEntry(e.value, e.key) })
+                    }
+                })
+                val poscol = readonlyColumn("fingerpos", UiTableEntry::fingerPos)
+                readonlyColumn("entry", UiTableEntry::tableEntry)
+                setOnMouseClicked {
+                    runAsyncWithOverlay {
+                        val list = node.listRequest(this@tableview.selectedItem!!.tableEntry)
 
-            //runAsync { refresh() }
-            sortOrder.add(poscol)
+                        (right as TableView<MyPair<String, String?>>).items =
+                            FXCollections.observableArrayList(list.map { MyPair<String, String?>(it, null) })
+                    }
+                }
+
+                //runAsync { refresh() }
+                sortOrder.add(poscol)
+            }
+            tableview<DataEntry> {
+                items = FXCollections.observableArrayList()
+                datamap.addListener(MapChangeListener {
+                    runLater {
+                        items = FXCollections.observableArrayList(datamap.map { e -> DataEntry(e.key, e.value) })
+                    }
+                })
+                readonlyColumn("name", DataEntry::name)
+                readonlyColumn("data", DataEntry::data)
+            }
         }
-        right = tableview<DataEntry> {
-            items = FXCollections.observableArrayList()
-            datamap.addListener(MapChangeListener {
-                runLater { items = FXCollections.observableArrayList(datamap.map { e -> DataEntry(e.key, e.value) }) }
-            })
-            readonlyColumn("name", DataEntry::name)
-            readonlyColumn("data", DataEntry::data)
-        }
+//        right =
         bottom =
             hbox {
                 val Putbox = vbox {
@@ -81,7 +108,9 @@ class ChordRing : View("ChordRing") {
                     setOnAction {
                         val key = Putbox.children[0] as TextArea
                         val data = Putbox.children[1] as TextArea
-                        node.putRequest(key.text, data.text)
+                        runAsync {
+                            node.putRequest(key.text, data.text)
+                        }
                     }
                 }
 
@@ -90,7 +119,7 @@ class ChordRing : View("ChordRing") {
 
                     }
                     val value = textarea {
-
+                        isEditable = false
                     }
                 }
 
@@ -99,7 +128,10 @@ class ChordRing : View("ChordRing") {
                     setOnAction {
                         val key = Getbox.children[0] as TextArea
                         val value = Getbox.children[1] as TextArea
-                        value.text = node.getRequest(key.text).toString()
+                        runAsync {
+                            value.text = node.getRequest(key.text).toString()
+                        }
+
                     }
                 }
             }
