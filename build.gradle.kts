ceb87d4dae049a1eb6458424c1577f338e2b6041
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-10-08 00:43:21 +0200

    Configured gradle for grpc-kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,20 +25,26 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
+    //grpc
     implementation("io.grpc:grpc-netty-shaded:1.26.0")
     implementation("io.grpc:grpc-protobuf:1.26.0")
     implementation("io.grpc:grpc-stub:1.26.0")
 
+    implementation("io.grpc:grpc-kotlin-stub:0.2.0")
+
+    // logging
     implementation("io.github.microutils:kotlin-logging:1.7.9")
     implementation("org.slf4j:slf4j-simple:1.7.29")
 
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-
+    //tornadofx
     implementation("no.tornado:tornadofx:1.7.17")
 
+    // coroutine
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.3.9")
 
+    //testing
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
     testImplementation("ch.tutteli.atrium:atrium-fluent-en_GB:0.12.0")
 }
@@ -48,6 +54,15 @@ protobuf {
         artifact = "com.google.protobuf:protoc:3.11.0"
     }
     plugins {
+        id("grpc-kt") {
+            /**
+             * Todo
+             * Should be version 0.2.0:jdk7@jar and the protobuff pulgin should be 0.8.13
+             * but when using this protobuff plugin, gradle can't resolve the coroutine
+             * dependency for some reason.
+             */
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:0.1.5"
+        }
         id("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.26.0"
         }
@@ -57,6 +72,7 @@ protobuf {
             it.plugins {
                 // Apply the "grpc" plugin whose spec is defined above, without options.
                 id("grpc")
+                id("grpc-kt")
             }
         }
     }
