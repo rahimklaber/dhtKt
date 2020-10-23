a67b72fe6d8cbe31a6b1b3610cb6b6a64384a3da
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-10-23 14:39:57 +0200

    Added Inmemory implementation of datatable.

--- src/main/kotlin/me/rahimklaber/dhtrpc/datatable/DataTable.kt
+++ src/main/kotlin/me/rahimklaber/dhtrpc/datatable/DataTable.kt
@@ -1,9 +1,15 @@
 package me.rahimklaber.dhtrpc.datatable
 
 interface DataTable<K,V> {
+    /**
+     * when data is inserted, the callbacks are ran.
+     * Therefore the callbacks should be lightweight.
+     */
     operator fun set(key: K, data: V)
     operator fun get(key: K): V?
-
+    /**
+     * register a callback to be run when data is inserted.
+     */
     fun registerSetCallback(callBack: SetCallBack<K,V>)
 
 }
---
+++ src/main/kotlin/me/rahimklaber/dhtrpc/datatable/SetCallBack.kt
@@ -0,0 +1,3 @@
+package me.rahimklaber.dhtrpc.datatable
+
+typealias SetCallBack<K,V> = (key: K, data: V) -> Unit
